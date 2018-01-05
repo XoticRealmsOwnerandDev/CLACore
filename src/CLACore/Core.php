c55cc5db0ef9f9e1c1901786c5639c4b79054a29
xXNiceYT <xXNiceAssasinl0@users.noreply.github.com>
2018-01-05 13:30:33 -0500

    Added Rank System

--- src/CLACore/Core.php
+++ src/CLACore/Core.php
@@ -9,6 +9,7 @@ use pocketmine\utils\Textformat as C;
 
 #Events
 use Events\onLoginEvent;
+use Ranks\Rank;
 
 class Core extends PluginBase{
 
@@ -25,10 +26,14 @@ class Core extends PluginBase{
     public function onConfig(){
         @mkdir($this->getDataFolder());
         $this->saveResource("config.yml");
+        $this->saveResource("rank.yml");
         $this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     }
 
     public function onEvent(){
+        if($this->cfg->get("Allow-Rank") == true){
+            $this->getServer()->getPluginManager()->registerEvents(($this->Rank = new Rank($this)), $this);
+        }
         $this->getServer()->getPluginManager()->registerEvents(($this->onLoginEvent = new onLoginEvent($this)), $this);
     }
 }
