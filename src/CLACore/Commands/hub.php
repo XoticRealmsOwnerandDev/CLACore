90f22a8216f479ab6e35bcc0185cab4043fa7800
AppleNulia <32245068+AppleNulia@users.noreply.github.com>
2018-01-09 21:42:05 +0100

    Added Hub Command

---
+++ src/CLACore/Commands/hub.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CLACore\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\Player;
+
+class hub extends Command{
+	
+	public function __construct(){
+        parent::__construct("hub");
+        $this->setDescription("Teleport to Hub");
+        $this->setAliases(["hub"]);
+	}
+	
+	public function execute(CommandSender $sender, string $label, array $args){
+        if (!$this->testPermission($sender)){
+            return true;
+        }
+                $sender->sendMessage("§fW§ee§al§cc§bo§dm§fe §bb§aa§ec§ck§7......");
+                $sender->teleport($sender->getlevel()->getSpawnLocation());
+            return true;
+        }
+    }
